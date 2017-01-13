c6aa9e657a0905c000d8d946ab4e2339f2b8202a
Jake Curran <jake@jakecurran.com>
2017-01-13 16:40:15 -0500

    Remove Log4j and all logging functionality

--- src/main/java/com/jakecurran/mavenbook/weather/WeatherRetriever.java
+++ src/main/java/com/jakecurran/mavenbook/weather/WeatherRetriever.java
@@ -4,24 +4,16 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
-import org.apache.log4j.Logger;
 
 public class WeatherRetriever {
-  private static final String darkSkyKey = "DARKSKY_API_KEY";
-
-  private static Logger log = Logger.getLogger(WeatherRetriever.class);
+  private static final String darkSkyKey = "DARK_SKY_API_KEY";
 
   public InputStream retrieve(Location location) throws Exception {
-    log.info("\tWeatherRetriever.retrieve()");
-
     String url = "https://api.darksky.net/forecast/" + darkSkyKey + "/" +
                  location.getLatitude() + "," + location.getLongitude()
                  + "?units=ca";
-
     URLConnection connection = new URL(url).openConnection();
-    log.info("\t\tOpen connection at URL: " + url);
 
-    log.info("\t\tRETURN: InputStream containing weather JSON from Dark Sky");
     return connection.getInputStream();
   }
 }
