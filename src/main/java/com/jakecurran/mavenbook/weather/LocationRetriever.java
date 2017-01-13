7be138fe49a472ed01271c79f9cbab07d1021759
Jake Curran <jake@jakecurran.com>
2017-01-13 17:04:23 -0500

    Encode input address paramter for URL

--- src/main/java/com/jakecurran/mavenbook/weather/LocationRetriever.java
+++ src/main/java/com/jakecurran/mavenbook/weather/LocationRetriever.java
@@ -3,12 +3,13 @@ package com.jakecurran.mavenbook.weather;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class LocationRetriever {
   private static final String geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?";
 
   public InputStream retrieve(String address) throws Exception {
-    String url = geocodeURL + "address=" + address;
+    String url = geocodeURL + "address=" + URLEncoder.encode(address, "UTF-8");
     URLConnection connection = new URL(url).openConnection();
 
     return connection.getInputStream();
